168aec37c449eaa02c7a977fc1bed5af57d89925
Danex2 <khadane.miller@gmail.com>
2018-06-11 09:57:20 -0400

    removed sensitive information

--- src/components/Twitchy.js
+++ src/components/Twitchy.js
@@ -7,7 +7,7 @@ export default class Twitchy extends React.Component {
         super(props);
         this.state = {
             streamer: '',
-            game: '',
+            game: 'Overwatch',
             viewers: 0,
             url: ''
         }
@@ -15,28 +15,7 @@ export default class Twitchy extends React.Component {
 
     //passing state into this doesn't though?
     componentDidMount() {
-        const instance = axios.create({
-            baseURL: 'https://api.twitch.tv/kraken/streams/',
-            timeout: 1000,
-            headers: {
-                'Accept': 'application/vnd.twitchtv.v5+json',
-                'Client-ID': 'dlu1hinslk0p1ekafzbgm6d2jlq5ow'
-            }
-        });
-
-        instance.get('?game=Overwatch&limit=100').then((response) => {
-            const streamer_array = []
-            for (let i = 0; i < response.data.streams.length; i++) {
-                if (response.data.streams[i].viewers > this.state.viewers && response.data.streams[i].broadcast_platform === 'live') {
-
-                    streamer_array.push(response.data.streams[i].channel.name);
-                }
-
-            }
-            const random_streamer = streamer_array[Math.floor(Math.random() * streamer_array.length)];
-            this.setState({ streamer: random_streamer });
-            this.setState({ url: `http://player.twitch.tv/?channel=${this.state.streamer}&muted=false` });
-        })
+        this.setState({ url: "http://player.twitch.tv/?channel=dallas&muted=false" })
     }
     handleRandomClick = (e) => {
         e.preventDefault();
@@ -45,11 +24,11 @@ export default class Twitchy extends React.Component {
             timeout: 1000,
             headers: {
                 'Accept': 'application/vnd.twitchtv.v5+json',
-                'Client-ID': 'dlu1hinslk0p1ekafzbgm6d2jlq5ow'
+                'Client-ID': '<client-id here>'
             }
         });
 
-        instance.get('?game=Overwatch&limit=100').then((response) => {
+        instance.get(`?game=${this.state.game}&limit=100`).then((response) => {
             const streamer_array = []
             for (let i = 0; i < response.data.streams.length; i++) {
                 if (response.data.streams[i].viewers > this.state.viewers && response.data.streams[i].broadcast_platform === 'live') {

718f18086764d2172ba1d8f8b769bdd5faa432cc
Danex2 <khadane.miller@gmail.com>
2018-06-10 21:08:12 -0400

    stream loads on page load and when submit button is pressed

--- src/components/Twitchy.js
+++ src/components/Twitchy.js
@@ -27,7 +27,32 @@ export default class Twitchy extends React.Component {
         instance.get('?game=Overwatch&limit=100').then((response) => {
             const streamer_array = []
             for (let i = 0; i < response.data.streams.length; i++) {
-                if (response.data.streams[i].viewers > 0 && response.data.streams[i].broadcast_platform === 'live') {
+                if (response.data.streams[i].viewers > this.state.viewers && response.data.streams[i].broadcast_platform === 'live') {
+
+                    streamer_array.push(response.data.streams[i].channel.name);
+                }
+
+            }
+            const random_streamer = streamer_array[Math.floor(Math.random() * streamer_array.length)];
+            this.setState({ streamer: random_streamer });
+            this.setState({ url: `http://player.twitch.tv/?channel=${this.state.streamer}&muted=false` });
+        })
+    }
+    handleRandomClick = (e) => {
+        e.preventDefault();
+        const instance = axios.create({
+            baseURL: 'https://api.twitch.tv/kraken/streams/',
+            timeout: 1000,
+            headers: {
+                'Accept': 'application/vnd.twitchtv.v5+json',
+                'Client-ID': 'dlu1hinslk0p1ekafzbgm6d2jlq5ow'
+            }
+        });
+
+        instance.get('?game=Overwatch&limit=100').then((response) => {
+            const streamer_array = []
+            for (let i = 0; i < response.data.streams.length; i++) {
+                if (response.data.streams[i].viewers > this.state.viewers && response.data.streams[i].broadcast_platform === 'live') {
 
                     streamer_array.push(response.data.streams[i].channel.name);
                 }
@@ -51,9 +76,13 @@ export default class Twitchy extends React.Component {
                         scrolling="no"
                         allowFullScreen="true">
                     </iframe>
-                    <div className="user-input">
-                        <button onClick={this.handleRandomClick}>Random streamer</button>
-                    </div>
+                </div>
+                <div className="user-input">
+                    <form onSubmit={this.handleRandomClick}>
+                        <button type='submit'>Random streamer</button>
+                        <input type='text' placeholder='game' />
+                        <input type='text' placeholder='viewer count' />
+                    </form>
                 </div>
             </div>
         );
